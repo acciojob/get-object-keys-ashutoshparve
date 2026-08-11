9ab67a2bea51917ea22f80a07847c018da59c021
ASHUTOSH PARVE <ashutosh.parve03@gmail.com>
2026-08-11 16:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //your JS code here. If required.
 let student={
-	name:"ashutosh";
-}
+	name:"ashutosh",
+};
 
 function getKeys(obj) {
 	return Object.keys(obj);
 }
 console.log(getKeys(student));
 	
-}
